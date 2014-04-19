570f479a7208b370fc0d34e50783a63e1423b8ae
brandontricci <brandonricci@shaw.ca>
2014-04-18 22:57:23 -0700

    changing colors

--- contact.php
+++ contact.php
@@ -1,147 +1,51 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+// check for form submission - if it doesn't exist then send back to contact form
+if (!isset($_POST['save']) || $_POST['save'] != 'contactform') {
+    header('Location: http://debutproducts.com/contact-form-submission.php'); exit;
+}
+// get the posted data
+$name = $_POST['contactform'];
+$email_address = $_POST['contact_email'];
+$phone = $_POST['contact_phone'];
+$message = $_POST['contact_message'];
+$field_email = $_POST['cf_email'];
 
-    <title>Debut Products - Buyer's Dashboard</title>
+// check that a name was entered
+if (empty($name))
+    $error = 'You must enter your name.';
+// check that an email address was entered
+elseif (empty($email_address)) 
+    $error = 'You must enter your email address.';
+// check for a valid email address
+elseif (!preg_match('/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/', $email_address))
+    $error = 'You must enter a valid email address.';
+// check that a phone number was entered
+if (empty($phone))
+    $error = 'You must enter your phone number.';
+// check that a message was entered
+elseif (empty($message))
+    $error = 'You must enter a message.';
+		
+// check if an error was found - if there was, send the user back to the form
+if (isset($error)) {
+    header('Location: http://debutproducts.com/contact-form-submission.php?e='.urlencode($error)); exit;
+}
 
-    <!-- Bootstrap core CSS -->
-    <link href="css/bootstrap.css" rel="stylesheet">
+$headers = "From: $email_address\r\n"; 
+$headers .= "Reply-To: $email_address\r\n";
 
-    <!-- Add custom CSS here -->
-    <link href="css/modern-business.css" rel="stylesheet">
-    <link href="font-awesome/css/font-awesome.min.css" rel="stylesheet">
-    
-</head>
+// write the email content
+$email_content = "Name: $name\n";
+$email_content .= "Email Address: $email_address\n";
+$email_content .= "Phone Number: $phone\n";
+$email_content .= "Message:\n\n$message";
+	
+// send the email
+//ENTER YOUR INFORMATION BELOW FOR THE FORM TO WORK!
+mail ('brandonricci@shaw.ca', 'Debut Products - Contact Form Submission', $email_content, $headers);
+	
+// send the user back to the form
+header('Location: contact.html?s='.urlencode('Thank you for your message.')); exit;
 
-<body>
-
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <!-- You'll want to use a responsive image option so this logo looks good on devices - I recommend using something like retina.js (do a quick Google search for it and you'll find it) -->
-                <a class="navbar-brand" href="index.html"><img src="img/Debut_Products_Logo.png" width="50px" height="24px">Debut Products</a>
-            </div>
-
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse navbar-ex1-collapse">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="about.html">About</a>
-                    </li>
-                    <li><a href="buyersdashboard.html">Buyer's Dashboard</a>
-                    </li>
-                    <li><a href="wholesale.html">Wholesale</a>
-                    </li>
-                     <li class="active"><a href="contact.html">Contact</a>
-                    </li>
-                     <li><a href="cart.html">View Cart</a>
-                    </li>
-                </ul>
-            </div>
-        </div><!-- /.navbar-collapse -->
-      </div><!-- /.container -->
-    </nav>
-
-    <!-- Page Content -->
-
-    <div class="container">
-      
-      <div class="row">
-      
-        <div class="col-lg-12">
-          <h1 class="page-header">Find a Retailer&nbsp;&nbsp;<small>We'd Love to Hear From You!</small></h1>
-          <ol class="breadcrumb">
-            <li><a href="index.html">Home</a></li>
-            <li class="active">Contact</li>
-          </ol>
-        </div>
-        
-        <div class="col-lg-12">
-          <!-- Embedded Google Map using an iframe - to select your location find it on Google maps and paste the link as the iframe src. If you want to use the Google Maps API instead then have at it! -->
-          <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="http://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
-        </div>
-
-      </div><!-- /.row -->
-      
-      <div class="row">
-
-        <div class="col-sm-8">
-          <h3>Let's Get In Touch!</h3>
-          <p>Are you a retailer? Do you want to carry these fabulous lines of product in your store? please send us a message!</p>
-    
-            <form role="form" name="cf_email" method="POST" action="contact-form-submission.php">
-              <div class="row">
-                <div class="form-group col-lg-4">
-                  <label for="input1">Name</label>
-                  <input type="text" name="contact_name" class="form-control" id="input1">
-                </div>
-                <div class="form-group col-lg-4">
-                  <label for="input2">Email Address</label>
-                  <input type="email" name="contact_email" class="form-control" id="input2">
-                </div>
-                <div class="form-group col-lg-4">
-                  <label for="input3">Phone Number</label>
-                  <input type="phone" name="contact_phone" class="form-control" id="input3">
-                </div>
-                <div class="clearfix"></div>
-                <div class="form-group col-lg-12">
-                  <label for="input4">Message</label>
-                  <textarea name="contact_message" class="form-control" rows="6" id="input4"></textarea>
-                </div>
-                <div class="form-group col-lg-12">
-                  <input type="hidden" name="save" value="contact">
-                  <button type="submit" class="btn btn-primary">Submit</button>
-                </div>
-              </div>
-            </form>
-        </div>
-
-        <div class="col-sm-4">
-          <h3>Debut Products<img src="img/Debut_Products_Logo.png" width="50px" height="24px"></h3>
-          <h4>A natural product wholesaler</h4>
-          <p>
-            Postal Code: V7T 1B8<br>
-            City: West Vancouver<br>
-            Country : Canada
-          </p>
-          <p><i class="fa fa-phone"></i> <abbr title="Phone">Phone</abbr> :(604) 362-1551</p>
-          <p><i class="fa fa-print"></i> <abbr title="Fax">Fax</abbr> :(604) 922-4944</p>
-          <p><i class="fa fa-envelope-o"></i> <abbr title="Email">E</abbr>: <a href="mailto:info@debutproducts.com">info@debutproducts.com</a></p>
-          <p><i class="fa fa-clock-o"></i> <abbr title="Hours">H</abbr>: Monday - Friday: 9:00 AM to 9:00 PM</p>
-          <ul class="list-unstyled list-inline list-social-icons">
-            <li class="tooltip-social facebook-link"><a href="#facebook-page" data-toggle="tooltip" data-placement="top" title="Facebook"><i class="fa fa-facebook-square fa-2x"></i></a></li>
-            <li class="tooltip-social linkedin-link"><a href="#linkedin-company-page" data-toggle="tooltip" data-placement="top" title="LinkedIn"><i class="fa fa-linkedin-square fa-2x"></i></a></li>
-            <li class="tooltip-social twitter-link"><a href="#twitter-profile" data-toggle="tooltip" data-placement="top" title="Twitter"><i class="fa fa-twitter-square fa-2x"></i></a></li>
-            <li class="tooltip-social google-plus-link"><a href="#google-plus-page" data-toggle="tooltip" data-placement="top" title="Google+"><i class="fa fa-google-plus-square fa-2x"></i></a></li>
-          </ul>
-        </div>
-
-      </div><!-- /.row -->
-
-    </div><!-- /.container -->
-
-    <div class="container">
-
-
-      <footer>
-      </footer>
-
-    </div><!-- /.container -->
-
-    <!-- JavaScript -->
-    <script src="js/jquery-1.10.2.js"></script>
-    <script src="js/bootstrap.js"></script>
-    <script src="js/modern-business.js"></script>
-
-  </body>
-</html>
+?>
